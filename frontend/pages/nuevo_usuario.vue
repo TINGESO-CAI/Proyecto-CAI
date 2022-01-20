e459727f2493668f00b02db89b5b4e6ba43ab106
JM <jose.ibaceta@usach.cl>
2022-01-20 19:58:49 -0300

    fix nuevo usuario

--- frontend/pages/nuevo_usuario.vue
+++ frontend/pages/nuevo_usuario.vue
@@ -98,12 +98,27 @@ export default {
     }
   },
   methods:{
+    validaRut : function (rutCompleto) {
+		if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+			return false;
+		var tmp 	= rutCompleto.split('-');
+		var digv	= tmp[1]; 
+		var rut 	= tmp[0];
+		if ( digv == 'K' ) digv = 'k' ;
+		return (this.dv(rut) == digv );
+    },
+    dv : function(T){
+      var M=0,S=1;
+      for(;T;T=Math.floor(T/10))
+        S=(S+T%10*(9-M++%6))%11;
+      return S?S-1:'k';
+    },
     registrarse:async function(){
       if(this.rut=='' || this.nombre=='' || this.apellido=='' || this.mail=='' || this.password=='' || this.confirmPassword==''){
         alert('Debe ingresar todos los campos.')
         return 0
       }
-      if(this.rut.split('-').length!=2){
+      if(this.validaRut(this.rut)==false){
         alert("Ingrese el rut de manera correcta.")
         return 0
       }
